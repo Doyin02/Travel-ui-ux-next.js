6a4ff6142b435a79ca15f8458f7bed16bd952b80
Adedoyin Tioluwanimi <adedoyintioluwanimi@gmail.com>
2024-01-04 15:21:10 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  images: {
+    unoptimized: true,
+    loader: 'default', 
+  },
 }
 
 module.exports = nextConfig
